bce6e7bee341fe315a68a36f646a8932647ca5f0
ajayaj0302 <ajayaj0302@gmail.com>
2022-04-22 23:53:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./controller/errorController");
+
+const userRouter = require("./routes/userRoutes");
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from the server",
+  });
+});
+
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
