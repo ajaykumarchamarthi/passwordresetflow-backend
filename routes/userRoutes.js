608e05d9ab9fa40873556213456f097010ad1bec
ajayaj0302 <ajayaj0302@gmail.com>
2022-04-23 14:45:52 +0530

    AppError module fixed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,4 @@
-const authController = require("../controller/authController");
+const authController = require("./../controller/authController");
 const express = require("express");
 const router = express.Router();
 
